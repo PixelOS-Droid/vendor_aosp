60bc8f5d601d2794e90fdd5bc1bb2df8c911dbaf
Gustavo Mendes <gusttavo.me@outlook.com>
2022-09-15 13:20:12 -0300

    vendor: Build GameSpace
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- config/common.mk
+++ config/common.mk
@@ -166,6 +166,11 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/%/libfuse-lite.so \
     system/%/libntfs-3g.so
 
+
+# Game Space
+PRODUCT_PACKAGES += \
+    GameSpace
+
 # Openssh
 PRODUCT_PACKAGES += \
     scp \
